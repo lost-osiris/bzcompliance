342d74b02e94c327260fb94870953ae0629273b7
lost-osiris <farva74@gmail.com>
2014-06-04 14:58:55 -0400

    changing appname

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup
 
 setup(
-    name='YourAppName',
+    name='bzcompliance',
     version='1.0',
     description='OpenShift App',
-    author='Your Name',
-    author_email='example@example.com',
+    author='Matthew Owens',
+    author_email='mowens@redhat.com',
     url='http://www.python.org/sigs/distutils-sig/',
     install_requires=['Django<=1.4'],
 )
